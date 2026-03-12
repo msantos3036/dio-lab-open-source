e9a8e44fb8ac6106b3f4756ca5d3a6b8ae735fa3
Marcelo Santos <98530133+msantos3036@users.noreply.github.com>
2026-03-12 17:36:35 -0300

    Update main.go

--- projetos/go-pin-pan/main.go
+++ projetos/go-pin-pan/main.go
@@ -1,1 +1,36 @@
+package main
 
+import "fmt"
+
+func parte1() {
+	fmt.Println("Números divisíveis por 3:")
+
+	for i := 1; i <= 100; i++ {
+		if i%3 == 0 {
+			fmt.Println(i)
+		}
+	}
+}
+
+func parte2() {
+	fmt.Println("\nJogo Pin Pan:")
+
+	for i := 1; i <= 100; i++ {
+
+		if i%3 == 0 {
+			fmt.Println("Pin")
+		} else if i%5 == 0 {
+			fmt.Println("Pan")
+		} else {
+			fmt.Println(i)
+		}
+
+	}
+}
+
+func main() {
+
+	parte1()
+	parte2()
+
+}
